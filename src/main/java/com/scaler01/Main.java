735c0156e89a15a2c8f6d79ef5fbbaf7af9c8397
abhijanbarik <cdhyper22@gmail.com>
2024-04-29 22:14:53 +0530

    fix 02

--- src/main/java/com/scaler01/Main.java
+++ src/main/java/com/scaler01/Main.java
@@ -1,2 +1,9 @@
-package com.scaler01;public class Main {
+package com.scaler01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME back to the game: ABHIJAN");
+
+    }
 }
