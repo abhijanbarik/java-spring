931104498c6d08dfbf6df70aa5176b6ca69d55e9
abhijanbarik <cdhyper22@gmail.com>
2024-04-30 21:45:08 +0530

    Day 2

--- src/main/java/com/scaler01/Main.java
+++ src/main/java/com/scaler01/Main.java
@@ -1,9 +1,14 @@
 package com.scaler01;
-
+import com.scaler01.greetings.Greetings;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("WELCOME back to the game: ABHIJAN");
+//        System.out.println("WELCOME back to the game: ABHIJAN");
 
+        /*********************************************************************************/
+
+        Greetings seeTime = new Greetings();
+
+        System.out.println(seeTime.greet());
     }
 }
---
+++ src/main/java/com/scaler01/greetings/Greetings.java
@@ -0,0 +1,32 @@
+package com.scaler01.greetings;
+
+public class Greetings {
+
+    /*
+    *
+    * Write a function which prints the appropriate greeting message based on the time of the day.
+    *
+    * */
+
+    public static String greet() {
+
+        int hour = java.time.LocalTime.now().getHour();
+
+        if(hour >= 0 && hour < 12){
+
+            return "Good Morning!";
+
+        } else if(hour >= 12 && hour < 16){
+
+            return "Good Afternoon!";
+
+        } else if(hour >= 16 && hour < 20){
+
+            return "Good Evening!";
+
+        } else {
+            return "Good Night!";
+        }
+    }
+
+}
