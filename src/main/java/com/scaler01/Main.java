9de84e5a47b2f9ec3b5d4f18a0c68bb97cd76843
abhijanbarik <cdhyper22@gmail.com>
2024-05-15 20:39:08 +0530

    Day 4

--- src/main/java/com/scaler01/Main.java
+++ src/main/java/com/scaler01/Main.java
@@ -1,5 +1,6 @@
 package com.scaler01;
 import com.scaler01.greetings.Greetings;
+import com.scaler01.http.Client;
 public class Main {
     public static void main(String[] args) {
 
@@ -7,8 +8,16 @@ public class Main {
 
         /*********************************************************************************/
 
-        Greetings seeTime = new Greetings();
+//        Greetings seeTime = new Greetings();
+//
+//        System.out.println(seeTime.greet());
 
-        System.out.println(seeTime.greet());
+        /*********************************************************************************/
+
+        Client client = new Client();
+
+        String response = client.get("https://github.com/square/okhttp");
+
+        System.out.println(response);
     }
 }
---
+++ src/main/java/com/scaler01/http/Client.java
@@ -0,0 +1,31 @@
+
+package com.scaler01.http;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import java.io.IOException;
+
+public class Client {
+
+    OkHttpClient okHttpClient = new OkHttpClient(); // Instance Variable for our okHttpClient class
+
+    public String get(String url) {
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try{
+
+            return okHttpClient.newCall(request).execute().body().string();
+
+        } catch(IOException e){
+
+            System.out.println("Error in get request" + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
